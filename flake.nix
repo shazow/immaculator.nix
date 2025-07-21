3baf0c311a9439e6642a231cae1f421592c8f347
Andrey Petrov <andrey.petrov@shazow.net>
2025-07-21 14:13:52 -0700

    flake.nix: Refactor to export mkImmaculate

--- flake.nix
+++ flake.nix
@@ -6,52 +6,52 @@
 
   outputs = { self, nixpkgs, microvm }: let
     system = "x86_64-linux";
-    name = "immaculate";
-  in {
-    packages.${system} = {
-      default = self.packages.${system}.${name};
-      "${name}" = self.nixosConfigurations.${name}.config.microvm.declaredRunner;
+
+    defaultMicroVM = {
+      mem = 1024;
+      # hotplugMem = 2048; # cloud-hypervisor: Extra memory that can be added if there's memory pressure
+      storeOnDisk = true;
+      writableStoreOverlay = "/nix/.rw-store";
+      hypervisor = "qemu";
+      interfaces = [
+        {
+          type = "user";
+          id = "microvm1";
+          mac = "02:00:00:00:00:01";
+        }
+      ];
     };
 
-    nixosConfigurations = {
-      "${name}" = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          microvm.nixosModules.microvm
-          {
-            microvm = {
-              mem = 1024;
-              # hotplugMem = 2048; # cloud-hypervisor: Extra memory that can be added if there's memory pressure
-              storeOnDisk = true;
-              writableStoreOverlay = "/nix/.rw-store";
-              hypervisor = "qemu";
-              interfaces = [
-                {
-                  type = "user";
-                  id = "microvm1";
-                  mac = "02:02:00:00:00:01";
-                }
-              ];
-            };
-          }
-          (
-            # configuration.nix
-            { pkgs, lib, ... }: {
-              networking.hostName = name;
-              system.stateVersion = lib.trivial.release;
-              services.getty.autologinUser = "root";
-              nix = {
-                package = pkgs.nixVersions.latest;
-                extraOptions = ''
-                  experimental-features = nix-command flakes
-                '';
-              };
-
-              systemd.network.enable = true;
-            }
-          )
-        ];
+    defaultConfiguration = { pkgs, lib, ... }: {
+      system.stateVersion = lib.trivial.release;
+      nix = {
+        package = pkgs.nixVersions.latest;
+        settings.experimental-features = [ "nix-command" "flakes" ];
       };
+      systemd.network.enable = true;
+
+      services.getty.autologinUser = "root";
+    };
+
+    mkImmaculate = {
+      devShell ? null, # Derivation to include in the environment
+      configuration ? defaultConfiguration,
+      vm ? defaultMicroVM,
+    }: (nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        microvm.nixosModules.microvm
+        { microvm = vm; }
+        configuration
+        # Add devShell to the environment if provided
+        { environment.systemPackages = if devShell != null then [ devShell ] else []; }
+      ];
+    }).config.microvm.declaredRunner;
+  in {
+    inherit mkImmaculate;
+
+    packages.${system} = {
+      default = mkImmaculate {};
     };
   };
 }
